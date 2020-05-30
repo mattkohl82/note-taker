dee6e0583f384dbe46a978d35c9a62c782ad9109
mattkohl82 <mattkohl82@gmail.com>
2020-05-29 22:40:16 -0500

    pages connecting

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const { db } = require('./db/db');
+var path = require("path");
+
+app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    });
+
+app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+
+app.get('/api/db', (req, res) => {
+    res.json(db);
+  });
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
+
